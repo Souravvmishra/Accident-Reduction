1b8a841d1551338f62b0638a98c39540e1da89d7
SouravvMishra <souravvmishra@gmail.com>
2023-03-20 19:15:57 +0530

    First

---
+++ app.py
@@ -0,0 +1,80 @@
+from flask import Flask, jsonify, render_template, request
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import bcrypt
+
+app = Flask(__name__)
+
+engine = create_engine('sqlite:///users.db', echo=True)
+Session = sessionmaker(bind=engine)
+Base = declarative_base()
+
+# Define User model
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    fullName = Column(String, nullable=False)
+    email = Column(String, unique=True, nullable=False)
+    password = Column(String, nullable=False)
+    phNumber = Column(Integer, nullable=False)
+
+    def __repr__(self):
+        return f'<User {self.username}>'
+
+# Create database tables if they don't exist
+Base.metadata.create_all(engine)
+
+
+
+@app.route('/register', methods=['POST', "GET"])
+def register():
+    if request.method == "POST":
+    # Get data from request
+        fullName = request.form.get('fullName')
+        password = request.form.get('password')
+        email = request.form.get('email')
+        phNumber = request.form.get('phNumber')
+        
+        # Hash password
+        hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+        
+        # Insert new user into database
+        session = Session()
+        try : 
+            user = User(fullName = fullName,password=hashed_password, email = email,phNumber = phNumber)
+            session.add(user)
+            session.commit()
+        except:
+            session.rollback()
+            return jsonify({'error': 'Username already exists!'}), 409
+        finally:
+            session.close()
+        
+        # Return success message
+        return jsonify({'message': 'User registered successfully!'})
+    else : 
+        return render_template("index.html")
+
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/cards")
+def cards():
+    return render_template("cards.html")
+
+@app.route("/raise-issue")
+def raise_issue():
+    return render_template("issue.html")
+
+
+@app.route("/report-accident")
+def accident():
+    return render_template("accident.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
